27d7cba3334bf4388a815535d540a17c50e52779
atiahosainrupa <atiahossenrupa@gmail.com>
2024-05-02 20:48:56 +0600

    My first java assignment

---
+++ script.js
@@ -0,0 +1,74 @@
+// 1.How to connect Js and Html? 
+ 
+// Ans:
+
+{/* <body>
+
+<script src="./script.js">
+
+</script>
+</body>  */}
+
+
+// 2.How to Write Variables in Javascript? Write at least 3 Variables?
+
+
+// Ans:
+// var a = 80;
+// Let Fullname = "Atia hosain rupa";
+// const s = 9.456;
+
+
+// 3. What is the difference between let, var, and const variables?
+
+
+// Ans: let does not take the same variables repeatedly.
+    //  such as:
+
+//  let secoundnumber=30; //Number
+// let secoundnumber2=60; //Number
+// let secoundnumber3=20; //Number
+// console.log(secoundnumber);
+// console.log(secoundnumber2);
+// console.log(secoundnumber3);
+  
+// Ans: var takes the same variables repeatedly.
+    // such as:
+
+// var firstNumber=80; //Number
+// var firstNumber=70; //Number
+// var firstNumber=45; //Number
+// console.log(firstNumber);
+
+// Ans: const does not take the same variables repeatedly and can be assign value only once.
+    //  such as:
+
+// const thirdNumber=48; //Number
+// console.log(thirdNumber);
+
+
+// 4.Suppose you want to print your name on the console. How do you write that?
+
+
+//  Ans:
+//  let fullname= "Atia Hosain Rupa";
+// console.log(fullname);
+
+// // Consol my fullname has been written
+
+// 5.Suppose you want to print your name on the alert box. How do you write that?
+
+
+// Ans:
+// alert ("Atia Hosain Rupa")
+
+// // My fullname has been written of alert box
+ 
+// 6. Take- two variables, the first number, and the second number, and then print their sum on the console.
+
+
+// Ans:
+// let f = 50; //Number
+// let s = 74; //Number
+// let sum = s + f;
+// console.log(sum);
